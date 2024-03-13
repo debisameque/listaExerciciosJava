a66dac2779085848d4eeed28a52e6119cce0079e
debisameque <debisameque@gmail.com>
2024-03-12 22:14:03 -0300

    Organizando

---
+++ strings_vetores/Q23.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Q23 {
+    public static void q23(){
+    Scanner in = new Scanner(System.in);
+    System.out.println();
+    }
+    public static void main(String[] args) {
+        q23();
+    }
+}
